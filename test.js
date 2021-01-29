5c8af9fb86c408375390407757d17f2b787dce7c
Another john doe <iown@x.com>
2021-01-29 16:25:26 +0530

    node-crone

--- test.js
+++ test.js
@@ -1,13 +1,36 @@
 //BTC API 
-var Request = require("request");
-setInterval( function(){
+var Request = require('request');
+var fs = require('fs');
+var crone = require('node-cron');
+// setInterval( function(){
+
+// Request.get("https://api.cryptonator.com/api/ticker/btc-usd", (error, response) => {
+//     if(error) {
+//         return console.log(error);
+//     }
+//     //console.log('connected');
+//     //console.log(response.body);
+//     console.log(JSON.parse(response.body));
+// });
+// },10000);
+
+// crone.schedule('*/10 * * * * *', () => {
+//     console.log('5th second');
+//     Request.get("https://api.cryptonator.com/api/ticker/btc-usd", (error, response) => {
+//         if (error) {
+//             return console.log(error);
+//         }
+//         console.log(JSON.stringify(response.body));
+//         fs.appendFileSync('23.txt', '\r\n new data ');
+//         fs.appendFileSync('23.txt', JSON.stringify(response.body));
+//     });
+// })
 
 Request.get("https://api.cryptonator.com/api/ticker/btc-usd", (error, response) => {
-    if(error) {
-        return console.log(error);
-    }
-    //console.log('connected');
-    //console.log(response.body);
-    console.log(JSON.parse(response.body));
-});
-},10000);
+        if (error) {
+            return console.log(error);
+        }
+        console.log(JSON.stringify(response.body));
+        fs.appendFileSync('23.txt', '\r\n new data ');
+        fs.appendFileSync('23.txt', JSON.stringify(response.body));
+    });
